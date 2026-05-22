a38125408683674b37bc27753dd256774fa946e1
dineshnerkar <dineshnerkar9016@gmail.com>
2026-05-22 10:47:01 +0530

    us1 changes done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Postlistawait from './PostList/Postlistawait';
+import Accordion from './Accordian';
+
+function App() {
+
+  return (
+    <>
+        <Postlistawait/>
+        <Accordion/>
+    </>
+  )
+}
+
+export default App
